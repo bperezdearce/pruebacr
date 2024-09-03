4ec7dc92ad12df9a1ba07c004e928bc86ae5ba79
Bárbara Pérez de Arce <barbara.perezdearce@gmail.com>
2024-09-02 23:17:07 -0400

    iuh

--- back/src/routes/products.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getProductById, getProducts } from "../controllers/product.controller";
-
-const router = Router();
-
-router.get("/", getProducts);
-router.get("/:id", getProductById);
-
-export default router;
